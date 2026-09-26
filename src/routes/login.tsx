23c6f6ab57baa253fcb2934f8d835fc1b25cc14e
Musadiq12 <93664542+Musadiq12@users.noreply.github.com>
2026-09-26 21:42:10 +0530

    chore: remove unused auth error import

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { siteConfig } from "@/config/site";
-import { friendlyError } from "@/lib/admin";
 import { loginServer } from "@/server/auth.functions";
 
 type Search = { redirect?: string | undefined };
